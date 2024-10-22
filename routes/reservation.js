41ca45d6fd4a209841eb20a7d28e280b8d4fea0c
dede133 <92465078+dede133@users.noreply.github.com>
2024-10-22 12:30:07 +0200

    Ajustes al endpoint de reservation

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const reservationController = require('../controllers/reservationController');
 
 // Rutas relacionadas con las reservas
-router.post('/make', reservationController.makeReservation);
+router.post('/reservation', reservationController.makeReservation);
 router.delete('/cancel/:reservation_id', reservationController.cancelReservation);
 router.put('/update-status/:reservation_id', reservationController.updateReservationStatus);
+router.get('/field/:field_id', reservationController.getReservationsByField);
+router.get('/field/:field_id/by-date', reservationController.getReservationsByFieldAndDate);
+
 
 module.exports = router;
